c713326c5366c2321159913a4b2a1cbe1d69f7e7
Flávio Andrade <flavioandrade0201@gmail.com>
2020-03-27 03:37:02 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,52 @@
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, FlatList, Alert} from 'react-native';
+import Header from './components/Header';
+import uuid from 'uuid-random';
+import ListItem from './components/ListItem';
+import AddItem from './components/AddItem';
+
+const App = () => {
+  const [items, setItems] = useState([
+    {id: uuid(), text: 'Milk'},
+    {id: uuid(), text: 'Eggs'},
+    {id: uuid(), text: 'Rice'},
+    {id: uuid(), text: 'Pasta'},
+  ]);
+
+  const deleteItem = id => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id != id);
+    });
+  };
+
+  const addItem = text => {
+    if (!text) {
+      Alert.alert('Error', 'Please enter an item');
+    } else {
+      setItems(prevItems => {
+        return [{id: uuid, text}, ...prevItems];
+      });
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Header />
+      <AddItem addItem={addItem} />
+      <FlatList
+        data={items}
+        renderItem={({item}) => (
+          <ListItem item={item} deleteItem={deleteItem} />
+        )}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
